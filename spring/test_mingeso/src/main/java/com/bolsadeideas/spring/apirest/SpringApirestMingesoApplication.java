7b5bd328ba909cb7d96efed09cb9ade66c273888
Sebastian Pinto <sebastian.pinto.g@usach.cl>
2018-04-22 12:38:10 -0300

    cambiando back a war

--- spring/test_mingeso/src/main/java/com/bolsadeideas/spring/apirest/SpringApirestMingesoApplication.java
+++ spring/test_mingeso/src/main/java/com/bolsadeideas/spring/apirest/SpringApirestMingesoApplication.java
@@ -2,11 +2,16 @@ package com.bolsadeideas.spring.apirest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.web.SpringServletContainerInitializer;
 
 @SpringBootApplication
-public class SpringApirestMingesoApplication {
+public class SpringApirestMingesoApplication extends SpringServletContainerInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringApirestMingesoApplication.class, args);
 	}
+	 protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	        return application.sources(SpringApirestMingesoApplication.class);
+	    }
 }
