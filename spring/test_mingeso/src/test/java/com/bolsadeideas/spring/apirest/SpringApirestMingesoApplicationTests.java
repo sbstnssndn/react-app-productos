02ed64c8623c253c1a3967edad0564e6e1d65ac6
Sebastian Pinto <sebastian.pinto.g@usach.cl>
2018-04-22 17:05:46 -0300

    prueba unitaria

--- spring/test_mingeso/src/test/java/com/bolsadeideas/spring/apirest/SpringApirestMingesoApplicationTests.java
+++ spring/test_mingeso/src/test/java/com/bolsadeideas/spring/apirest/SpringApirestMingesoApplicationTests.java
@@ -1,16 +1,38 @@
 package com.bolsadeideas.spring.apirest;
 
+import static org.junit.Assert.fail;
+
+import java.util.Date;
+
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import com.bolsadeideas.spring.apirest.models.entity.Producto;
+import com.bolsadeideas.spring.apirest.models.services.IProductoService;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SpringApirestMingesoApplicationTests {
 
+	@Autowired
+	IProductoService daoService;
+	
 	@Test
 	public void contextLoads() {
+		Producto producto = new Producto();
+		producto.setNombre("productoPrueba");
+		producto.setCategoria("categoriaPrueba");
+		producto.setPrecio(1000);
+		producto.setFechaVencimiento(new Date());
+		if(daoService.save(producto) == null) fail("no se pudo agregar");
+		
 	}
+	
+	
+
 
 }
